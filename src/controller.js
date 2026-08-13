0b3f6657b66b5b0f376744496cdc3ce5477227c5
cedric_daniel_chan@dlsu.edu.ph <cedric_daniel_chan@dlsu.edu.ph>
2026-08-13 21:35:34 +0800

    feat: Add fetching data functionality

--- src/controller.js
+++ src/controller.js
@@ -1,8 +1,18 @@
+import { getWeatherData } from "./model.js";
+
 const handleSubmit = () => {
   document.addEventListener("submit", (e) => {
     e.preventDefault();
     if (e.target.id === "mainForm") {
-      console.log("I have submitted");
+      const location = document.querySelector("input").value;
+      (async () => {
+        try {
+          const weatherData = await getWeatherData(location);
+          console.log("data: " + JSON.stringify(weatherData));
+        } catch (error) {
+          console.log("error: " + error);
+        }
+      })();
     }
   });
 };
--- src/model.js
+++ src/model.js
@@ -2,17 +2,15 @@
 For temp: us for Farenheit, metric for Celcius
 */
 
-const getWeatherData = async () => {
-  try {
-    const response = await fetch(
-      "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/manila/next5days?unitGroup=metric&key=S5TGJ858R683JZX54HDUDQ6JN&include=days&elements=temp,precip,humidity,windspeed,conditions,icon",
-    );
+const getWeatherData = async (location) => {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next5days?unitGroup=metric&key=S5TGJ858R683JZX54HDUDQ6JN&include=days&elements=temp,precip,humidity,windspeed,conditions,icon`,
+  );
 
-    const weatherData = await response.json();
-    console.log("Success in getting the data!");
-  } catch (error) {
-    console.log("Error:" + error);
-  }
+  if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+  const weatherData = await response.json();
+  return weatherData;
 };
 
 export { getWeatherData };
