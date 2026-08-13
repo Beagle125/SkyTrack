6b02756cfa25b766a08c85956764655b957a6c55
cedric_daniel_chan@dlsu.edu.ph <cedric_daniel_chan@dlsu.edu.ph>
2026-08-13 13:22:45 +0800

    feat: Add styles to the mainUpper div

--- src/display.js
+++ src/display.js
@@ -1,10 +1,29 @@
+import searchImage from "../static/search.svg";
+
 const loadMainUpper = (upperMain) => {
-  const header = document.createElement("p");
-  header.textContent = "Sky Track";
+  const header = document.createElement("div");
+  const sky = document.createElement("span");
+  const track = document.createElement("span");
+  sky.textContent = "Sky";
+  track.textContent = "Track";
+  header.classList.add("header");
+  sky.classList.add("highlighted");
+
+  header.appendChild(sky);
+  header.appendChild(track);
 
   const form = document.createElement("form");
   const input = document.createElement("input");
+  input.id = "formInput";
+  input.type = "text";
+  input.autocomplete = "off";
+  input.placeholder = "search a location...";
   const button = document.createElement("button");
+  button.type = "submit";
+  const buttonImg = document.createElement("img");
+  buttonImg.id = "searchImg";
+  buttonImg.src = searchImage;
+  button.appendChild(buttonImg);
 
   form.appendChild(input);
   form.appendChild(button);
