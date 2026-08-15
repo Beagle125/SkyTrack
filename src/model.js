a6a568956bdd12743fbb0622e861e3ab63b6f1a3
cedric_daniel_chan@dlsu.edu.ph <cedric_daniel_chan@dlsu.edu.ph>
2026-08-15 13:59:23 +0800

    refactor: Add datetime data to the json response

--- src/model.js
+++ src/model.js
@@ -4,7 +4,7 @@ For temp: us for Farenheit, metric for Celcius
 
 const getWeatherData = async (location, tempMeasurement) => {
   const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next5days?unitGroup=${tempMeasurement}&key=S5TGJ858R683JZX54HDUDQ6JN&include=days&elements=temp,precip,humidity,windspeed,conditions,icon`,
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next5days?unitGroup=${tempMeasurement}&key=S5TGJ858R683JZX54HDUDQ6JN&include=days&elements=datetime,temp,precip,humidity,windspeed,conditions,icon`,
   );
 
   if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
